f60c06da791c9db740cd06850ea317bdd48974ff
Sam Keddy <skeddy13@yahoo.com>
2024-04-30 17:44:51 -0400

    removed unnecessary get_node in hud script

--- scripts/hud-player.gd
+++ scripts/hud-player.gd
@@ -6,7 +6,7 @@ var arenaScene = find_parent("Level")
 
 func initialize(numberOfPlayers):
 	print("init hud",numberOfPlayers)
-	get_node("Contents/PlayerNumber").text = "P"+str(character.playerNumber)
+	$Contents/PlayerNumber.text = "P"+str(character.playerNumber)
 	add_theme_constant_override("margin_left", 50)
 	add_theme_constant_override("margin_right", 50)
 	$Contents/PlayerSprite.texture = character.get_node("Sprite").sprite_frames.get_frame_texture("idle",0)
@@ -19,8 +19,8 @@ func update():
 	if not character: return
 	
 	# Reset
-	get_node("Contents/NoBalloons").visible = false
-	get_node("Contents/Dead").visible = false
+	$Contents/NoBalloons.visible = false
+	$Contents/Dead.visible = false
 	for i in range(1,11):
 		get_node("Contents/Balloon"+str(i)).visible = false
 	setBalloonCounterVisibility(false)
@@ -28,13 +28,13 @@ func update():
 	# Show Dead Symbol
 	if (character.falling):
 		print("marking p",str(character.playerNumber)," as dead")
-		get_node("Contents/Dead").visible = true
+		$Contents/Dead.visible = true
 		return
 		
 	# Update Balloon Counter
 	if character.stored_balloons > 10:
 		print("more than 10 balloons")
-		get_node("Contents/BalloonCounter").text = "x"+str(character.stored_balloons)
+		$Contents/BalloonCounter.text = "x"+str(character.stored_balloons)
 		setBalloonCounterVisibility(true)
 	elif character.stored_balloons > 1:
 		print("1-10 balloons")
@@ -42,9 +42,9 @@ func update():
 			get_node("Contents/Balloon"+str(i)).visible = true
 	else:
 		print("0 balloons")
-		get_node("Contents/NoBalloons").visible = true
+		$Contents/NoBalloons.visible = true
 
 func setBalloonCounterVisibility(visibility):
-	get_node("Contents/BalloonCounter").visible = visibility
+	$Contents/BalloonCounter.visible = visibility
 	for i in range(1,6):
 		get_node("Contents/CountBalloon"+str(i)).visible = visibility
